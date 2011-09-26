dff8eb288f90f74de1a00ba86098e1dae3642840
David Nettle <david.nettle@gmail.com>
2011-09-27 02:39:24 +0900

    more stupid reorganising. i am not yet good at this.

---
+++ lib/db.rb
@@ -0,0 +1,4 @@
+require 'sequel'
+
+DB = Sequel.connect(:adapter => 'mysql2', :host => 'localhost', :database => 'transpharm', :user => 'transpharm', :password =>'transpharm', :encoding => 'utf8')
+$japmeds = DB[:japmeds]
